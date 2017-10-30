ddc63d2156dee1f2e8e5d225b58e4f2f22b4aaf2
Masato Narita <Das_hexenhaus@hotmail.co.jp>
2017-10-30 15:05:38 +0900

    practice01

---
+++ practice01.cs
@@ -0,0 +1,17 @@
+﻿/* 
+   簡単なC#サンプルプログラム
+
+   Example.cs
+*/
+
+using System;
+
+class Example
+{
+
+    // C# プログラムは  Main()メソッドから実行を開始する
+    static void Main()
+    {
+        Console.WriteLine("C# gives you programming power.");
+    }
+}
